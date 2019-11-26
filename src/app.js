b2b944669a832589052a2dd3948768e90e25f766
Anna Sh <anna.sh.pb@gmail.com>
2019-11-26 22:48:45 +0200

    mod10fixedabit

--- src/app.js
+++ src/app.js
@@ -2,8 +2,5 @@ import refreshList from './components/menu';
 import switchTheme from './components/theme';
 import data from './db/menu.json';
 import refs from './utils/refs';
-const checkbox = document.querySelector('#theme-switch-control');
 
 refreshList(data, refs.menu);
-
-checkbox.addEventListener('change', switchTheme);
--- src/components/theme.js
+++ src/components/theme.js
@@ -16,12 +16,16 @@ if (JSON.parse(currentTheme) === Theme.DARK) {
 
 const switchTheme = function(event) {
   if (event.target.checked) {
-    body.setAttribute('class', Theme.DARK);
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
     localStorage.setItem('theme', JSON.stringify(Theme.DARK));
   } else {
-    body.setAttribute('class', Theme.LIGHT);
+    body.classList.add(Theme.LIGHT);
+    body.classList.remove(Theme.DARK);
     localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
   }
 };
+
+checkbox.addEventListener('change', switchTheme);
 
 export default switchTheme;
